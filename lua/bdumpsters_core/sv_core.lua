7fe10a9e81113a976d52b3673896f71225c2e901
Ben-Thirkill <52956101+Ben-Thirkill@users.noreply.github.com>
2022-07-16 06:41:06 +0100

    Update sv_core.lua

--- lua/bdumpsters_core/sv_core.lua
+++ lua/bdumpsters_core/sv_core.lua
@@ -4,14 +4,14 @@ util.AddNetworkString("bdumpster_send_info")
 util.AddNetworkString("bdumpsters_msg")
 
 net.Receive("bdumpster_request_info", function(len, ply)
-	local index = net.ReadUInt(10)
+	local index = net.ReadUInt(6)
 	local ent = Entity(index)
 
 	if not IsValid(ent) or ent:GetClass() != "bdumpster" then return end 
 
     net.Start("bdumpster_send_info")
         net.WriteUInt(ent.cooldown, 32)
-        net.WriteUInt(ent:EntIndex(), 10)
+        net.WriteUInt(ent:EntIndex(), 6)
     net.Send(ply)
 end)
 
